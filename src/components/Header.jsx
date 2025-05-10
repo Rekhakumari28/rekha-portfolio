508927ae4f222138e2a26e178f8daf65770d4279
rekha kumari bheel <134380492+Rekhakumari28@users.noreply.github.com>
2025-05-10 14:42:20 +0530

    Update Header.jsx
    Change nav link name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ export function Header(){
         <nav className="navigation">
     <div className="nav-brand">Rekha Kumari Bheel</div>
     <ul className="list-non-bullet nav-pills">
-      <li className="list-item-inline"> <a className="link link-active" href="#hero">Introduction</a> </li>
+      <li className="list-item-inline"> <a className="link link-active" href="#hero">About</a> </li>
       <li className="list-item-inline"> <a className="link" href="#projects">Projects</a> </li>
-      <li className="list-item-inline"> <a className="link" href="#technologies">Technologies</a></li>
+      <li className="list-item-inline"> <a className="link" href="#technologies">Stack</a></li>
     </ul>
   </nav>
     )
