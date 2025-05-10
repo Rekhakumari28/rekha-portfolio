ee451e72cbc46ee996b59e8975a097d0a3ad357f
unknown <rekha.kumari1928@gmail.com>
2025-05-10 15:22:19 +0530

    added HashLink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
-import {Link} from "react-router-dom"
+import { HashLink } from 'react-router-hash-link';
 export function Header(){
     return(
         <nav className="navigation">
     <div className="nav-brand">Rekha Kumari Bheel</div>
     <ul className="list-non-bullet nav-pills">
-      <li className="list-item-inline"> <Link className="link link-active" href="#hero">About</Link> </li>
-      <li className="list-item-inline"> <Link className="link" href="#projects">Projects</Link> </li>
-      <li className="list-item-inline"> <Link className="link" href="#technologies">Stack</Link></li>
+      <li className="list-item-inline"> <HashLink className="link link-active" to="#hero">About</HashLink> </li>
+      <li className="list-item-inline"> <HashLink className="link" to="#projects">Projects</HashLink> </li>
+      <li className="list-item-inline"> <HashLink className="link" to="#technologies">Stack</HashLink></li>
     </ul>
   </nav>
     )
