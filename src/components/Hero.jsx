b379c1ddb3a6969f2a8f0b7451d6ca3e6597d81b
rekha kumari bheel <134380492+Rekhakumari28@users.noreply.github.com>
2025-05-10 14:37:13 +0530

    Update Hero.jsx
    Edit link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ export function Hero(){
            <img src={hero} alt="hero-img" className="hero-img" />
         <h1 className="hero-heading">Rekha Kumari Bheel</h1>
         <h2 className="hero-heading2 mb-3">A Full Stack Developer</h2>
-        <Link className="link link-primary text-white " to="#project">View My Work</Link>
+        <Link className="link link-primary text-white " to="#projects">View My Work</Link>
         
       </section> 
         </header>
