0ae5ddfb13c8baca9399ca18318b251fdc78f681
rekha kumari bheel <134380492+Rekhakumari28@users.noreply.github.com>
2025-05-10 14:49:49 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
+import {Link} from "react-router-dom"
 export function Header(){
     return(
         <nav className="navigation">
     <div className="nav-brand">Rekha Kumari Bheel</div>
     <ul className="list-non-bullet nav-pills">
-      <li className="list-item-inline"> <a className="link link-active" href="#hero">About</a> </li>
-      <li className="list-item-inline"> <a className="link" href="#projects">Projects</a> </li>
-      <li className="list-item-inline"> <a className="link" href="#technologies">Stack</a></li>
+      <li className="list-item-inline"> <Link className="link link-active" href="#hero">About</Link> </li>
+      <li className="list-item-inline"> <Link className="link" href="#projects">Projects</Link> </li>
+      <li className="list-item-inline"> <Link className="link" href="#technologies">Stack</Link></li>
     </ul>
   </nav>
     )
