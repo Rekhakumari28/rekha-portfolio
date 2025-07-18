3d8a922bf2b6d65358d3d7810d93e9cc832e0f86
rekha kumari bheel <134380492+Rekhakumari28@users.noreply.github.com>
2025-07-18 18:18:38 +0530

    pull request

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export function Footer(){
     return(
         <section className="mt-5 pt-5 text-center mb-5 pb-5">
           <p className="mb-3 ">&copy; 2025 Rekha Kumari Bheel • All rights reserved</p>
-          <p className="mb-3 ">Made with care using Bootstrap and React.j</p>   
+          <p className="mb-3 ">Made with care using Bootstrap and React.js</p>   
        
       </section>
     )
